cc03aad422acb0603fe5158ecda3a40f96c4f44c
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2023-10-05 20:09:59 +0200

    Nuevo metodo de scroll

--- lib/layout_joc.dart
+++ lib/layout_joc.dart
@@ -24,54 +24,36 @@ class LayoutJoc extends StatelessWidget {
   // ... aquí la funció ‘build’ definida a la següent diapositiva
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Ocupar la meitat superior de l'espai amb la imatge
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 100.0,
-            height: MediaQuery.of(context).size.height * 0.5,
-            child: Image.asset(
-              'assets/images/${itemData["imatge"]}',
-              fit: BoxFit.contain,
-            ),
+    return ListView(
+      children: [
+        // Ocupar la meitat superior de l'espai amb la imatge
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 100.0,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Image.asset(
+            'assets/images/${itemData["imatge"]}',
+            fit: BoxFit.contain,
           ),
-          // Ocupar la meitat inferior de l'espai amb els textos
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.4,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "${itemData['any']}",
-                    style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey),
-                  ),
-                  Text(
-                    "${itemData['tipus']}",
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 30),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0),
-                    child: Text(
-                      "${itemData['descripcio']}",
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal),
-                    ),
-                  )
-                ],
-              ),
-            ),
+        ),
+
+        Text(
+          "${itemData['any']}",
+          style: const TextStyle(
+              fontSize: 20, fontWeight: FontWeight.normal, color: Colors.grey),
+        ),
+        Text(
+          "${itemData['tipus']}",
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 30),
+        Padding(
+          padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0),
+          child: Text(
+            "${itemData['descripcio']}",
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
